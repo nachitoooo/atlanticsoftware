e73956c3f291a7d7448ce3de0c0be8ea9a8e75fc
nacho <ignacioaristegui025@gmail.com>
2024-03-20 09:55:43 -0300

    sticky scroll y esas cosas

--- src/components/component/sticky-scroll.tsx
+++ src/components/component/sticky-scroll.tsx
@@ -13,30 +13,30 @@ const content = [
     description:
       "Atlantic Software crafts custom software solutions for startups and companies, integrating AI to fuel growth. Specializing in e-commerce and landing pages, we elevate businesses and convert visitors into customers. Choose us to boost your business.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-        Collaborative Editing
-        <AiOutlineTeam className="ml-2" />
+      <div className="h-full w-full bg-gradient-to-br from-blue-900 to-gray-800 flex items-center justify-center text-white">
+        Our way of working
+        <AiOutlineTeam className="ml-2" size={18}/>
       </div>
     ),
   },
   {
     title: "Our first meeting",
     description:"The first step is to have a meeting to understand your needs and requirements. We will discuss the project and establish a budget.",
-          content: (
-            <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-            Meeting.
-            <HiVideoCamera className="ml-2" />
-          </div>
+    content: (
+      <div className="h-full w-full bg-gradient-to-br from-blue-800 to-gray-800 flex items-center justify-center text-white">
+        Meeting
+        <HiVideoCamera className="ml-2" size={18}/>
+      </div>
     ),
   },
   {
     title: "Start the project",
     description:
-      "Once our proposed budget and timeline are mutually agreed upon, the desing and development team at Atlantic Software will promptly initiate work on developing your idea.",
+      "Once our proposed budget and timeline are mutually agreed upon, the design and development team at Atlantic Software will promptly initiate work on developing your idea.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
+      <div className="h-full w-full bg-gradient-to-br from-blue-700 to-gray-800 flex items-center justify-center text-white">
         Start of the project
-        <MdOutlineNotStarted className="ml-2" />
+        <MdOutlineNotStarted className="ml-2" size={18} />
       </div>
     ),
   },
@@ -45,9 +45,9 @@ const content = [
     description:
       "At Atlantic Software, we believe in transparency and accountability throughout the project lifecycle. With our approach, you'll witness tangible progress on your project every week. Weekly meetings are scheduled to showcase the advancements made, ensuring clear communication and alignment with your vision.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-       Results
-       <FaNetworkWired className="ml-2" />
+      <div className="h-full w-full bg-gradient-to-br from-blue-600 to-gray-800 flex items-center justify-center text-white">
+        Results
+        <FaNetworkWired className="ml-2" size={18}/>
       </div>
     ),
   },
